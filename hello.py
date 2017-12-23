772dfa8b7672593175289d9c5545a28ef09499f4
sandstonestudio <ss2k.shu@outlook.com>
2017-12-23 09:55:14 +0800

    add name into db in index()

--- hello.py
+++ hello.py
@@ -54,15 +54,18 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
-    if form.validate_on_submit():        
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
