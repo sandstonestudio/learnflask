d34518d356f685136ae22e35d13bfcff66335154
sandstonestudio <ss2k.shu@outlook.com>
2018-03-26 15:36:39 +0800

    Chapter 11: Blog posts in profile pages (11b)

--- app/main/views.py
+++ app/main/views.py
@@ -16,14 +16,15 @@ def index():
                     author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()    
+    posts = Post.query.order_by(Post.timestamp.desc()).all()        
     return render_template('index.html', form=form, posts=posts)
 
 
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user, current_user=current_user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
