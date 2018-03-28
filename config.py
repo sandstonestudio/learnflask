03332bcace6b703f0f80fe6ab951d82d937535af
sandstonestudio <ss2k.shu@outlook.com>
2018-03-28 12:19:31 +0800

    Chapter 11: Generate fake users and posts (11c)

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <skyshu2000@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'skyshu2000@qq.com'
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
