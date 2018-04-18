c309cda6406b5c19bcd753af34ec7a0fb8419bdb
sandstonestudio <ss2k.shu@outlook.com>
2018-04-18 16:31:23 +0800

    Chapter 12: Followers in the application (12b)

--- app/models.py
+++ app/models.py
@@ -218,13 +218,13 @@ class User(UserMixin, db.Model):
     
     def follow(self, user):
         if not self.is_following(user):
-            f = Follow(followed=user)
-            self.followed.append(f)
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
 
     def unfollow(self, user):
         f = self.followed.filter_by(followed_id=user.id).first()
         if f:
-            self.followed.remove(f)
+            db.session.delete(f)
 
     def __repr__(self):
         return '<User %r>' % self.username
