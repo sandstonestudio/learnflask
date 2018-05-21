89df93043210cea4440e78b6cb636dd9a63e3a5d
sandstonestudio <ss2k.shu@outlook.com>
2018-05-21 09:27:22 +0800

    Chapter 14: API (14a)

--- app/models.py
+++ app/models.py
@@ -4,9 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from markdown import markdown
 import bleach
-from flask import current_app, request
+from flask import current_app, request, url_for
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+from app.exceptions import ValidationError
 
 # 以0x开头表示16进制
 class Permission:
@@ -242,6 +243,33 @@ class User(UserMixin, db.Model):
                                Follow.followed_id == Post.author_id
                               ).filter(Follow.follower_id == self.id)
 
+    def to_json(self):
+        json_user = {
+            'url': url_for('api.get_user', id=self.id, _external=True),
+            'username': self.username,
+            'member_since': self.member_since,
+            'last_seen': self.last_seen,
+            'posts': url_for('api.get_user_posts', id=self.id, _external=True),
+            'followed_posts': url_for('api.get_user_followed_posts',
+                                      id=self.id, _external=True),
+            'post_count': self.posts.count()
+        }
+        return json_user
+    
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], 
+                       expires_in=expiration)
+        return s.dumps({'id': self.id}).decode('ascii')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -294,6 +322,27 @@ class Post(db.Model):
         target.body_html = bleach.linkify(bleach.clean(
                                 markdown(value, output_format='html'),
                                 tags=allowed_tags, strip=True))
+    
+    def to_json(self):
+        json_post = {
+            'url': url_for('api.get_post', id=self.id, _external=True),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author': url_for('api.get_user', id=self.author_id,
+                              _external=True),
+            'comments': url_for('api.get_post_comments', id=self.id,
+                                _external=True),
+            'comment_count': self.comments.count()
+        }
+        return json_post
+    
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if (body is None) or (body == ''):
+            raise ValidationError('post does not have a body')
+        return Post(body=body)
 
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
