764b0dc5265e70bdad7a13377095006de10b9305
sandstonestudio <ss2k.shu@outlook.com>
2018-05-22 10:17:39 +0800

    Chapter 15: Unit tests with the Flask test client (15b)

--- config.py
+++ config.py
@@ -35,6 +35,7 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    WTF_CSRF_ENABLE = False
 
 
 class ProductionConfig(Config):
